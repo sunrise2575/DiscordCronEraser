5f5a31a876d656d1aac5e941813825e2d93267e1
Heeyong Yoon <sunrise2575@gmail.com>
2021-08-06 09:32:20 +0900

    fix bulk delete bug

--- bot-delete-all.go
+++ bot-delete-all.go
@@ -56,12 +56,24 @@ func treatDeleteAll(connInfo connInfoType, sess *discordgo.Session, msg *discord
 	}
 
 	{
-		// bulk delete 실행
-		if e := sess.ChannelMessagesBulkDelete(msg.ChannelID, messageIDs); e != nil {
-			if e := tx.Rollback(); e != nil {
-				panic(e)
+		// bulk delete 실행. 한번에 100개까지만 지우기 때문에 반복해서 지워야 한다
+		for i := 0; i < len(messageIDs); i += 100 {
+			if len(messageIDs)-i >= 100 {
+				if e := sess.ChannelMessagesBulkDelete(msg.ChannelID, messageIDs[i:i+100]); e != nil {
+					if e := tx.Rollback(); e != nil {
+						panic(e)
+					}
+					return
+				}
+			} else {
+				// 끝에 100개보다 덜 남은 경우
+				if e := sess.ChannelMessagesBulkDelete(msg.ChannelID, messageIDs[i:]); e != nil {
+					if e := tx.Rollback(); e != nil {
+						panic(e)
+					}
+					return
+				}
 			}
-			return
 		}
 	}
 
